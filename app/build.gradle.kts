d770696134fd24a9961841e8da4bd78edb51c2a2
JaronLouise <22-03531@g.batstate-u.edu.ph>
2024-11-22 23:45:33 +0800

    added splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //gif
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+
+    //firebase
+    implementation ("com.google.firebase:firebase-auth:21.1.0")
+    implementation ("com.google.firebase:firebase-database:20.0.3")
+    implementation ("com.google.firebase:firebase-core:20.1.0")
 }
